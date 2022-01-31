aafb61f68747416f7a05f3a86fc6bf226b0a110e
unknown <duy88tran@gmail.com>
2022-01-31 19:05:56 +0700

    'video3_2'

---
+++ learn-nextjs/pages/api/products/index.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  //if the method user uses is different from 'GET', the resul status is error 404 with the warning in prop name
+  if(req.method !== 'GET') {
+    return res.status(404).json({name: 'method is not supported'})
+  }
+  res.status(200).json({ name: 'Get list of products' })
+}
