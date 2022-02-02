eb819020e815c547632cae96764c101a5a7879c0
unknown <duy88tran@gmail.com>
2022-02-02 16:06:27 +0700

    'proxy-server'

--- learn-nextjs/pages/api/products/index.ts
+++ learn-nextjs/pages/api/products/index.ts
@@ -1,17 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+	name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  //if the method user uses is different from 'GET', the resul status is error 404 with the warning in prop name
-  if(req.method !== 'GET') {
-    return res.status(404).json({name: 'method is not supported'})
-  }
-  res.status(200).json({ name: 'Get list of products' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	//if the method user uses is different from 'GET', the resul status is error 404 with the warning in prop name
+	if (req.method !== 'GET') {
+		return res.status(404).json({ name: 'method is not supported' });
+	}
+	res.status(200).json({ name: 'Get list of products' });
 }
