becb5aa221176f4be1aeda78c8031354ed34ab8b
unknown <duy88tran@gmail.com>
2022-02-18 11:43:00 +0700

    '4_9'

--- learn-nextjs/components/common/header/header-desktop.tsx
+++ learn-nextjs/components/common/header/header-desktop.tsx
@@ -1,13 +1,15 @@
-import { Container, Stack, Link as MuiLink } from '@mui/material'; //cause Link is also imported from 'next/link', so we need to change Link into MuiLink to
+import { Container, Link as MuiLink, Stack } from '@mui/material'; //cause Link is also imported from 'next/link', so we need to change Link into MuiLink to
 //use it as an <a></a> tags
 import { Box } from '@mui/system';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import { ROUTE_LIST } from './routes';
+import clsx from 'clsx';
 
-export interface HeaderDesktopProps {}
+export function HeaderDesktop() {
+	const router = useRouter();
 
-export function HeaderDesktop(props: HeaderDesktopProps) {
 	return (
 		<Box display={{ xs: 'none', md: 'block' }} py={2}>
 			<Container>
@@ -15,7 +17,10 @@ export function HeaderDesktop(props: HeaderDesktopProps) {
 				<Stack direction="row" justifyContent="flex-end">
 					{ROUTE_LIST.map((route) => (
 						<Link href={route.path} key={route.path} passHref>
-							<MuiLink sx={{ ml: 2 }}>{route.label}</MuiLink>
+							<MuiLink sx={{ ml: 2 }} className={clsx({ active: router.pathname === route.path })}>
+								{/* if router.pathname === route.path => className='active' */}
+								{route.label}
+							</MuiLink>
 						</Link>
 					))}
 				</Stack>
